b268b9f80c6616f69368ae9d60a95adad855e35c
fabrice137 <fabruz3@gmail.com>
2023-03-08 11:51:55 +0200

    Initial commit

---
+++ src/main/java/com/haguma/FuelConvert/FuelConvertApplication.java
@@ -0,0 +1,13 @@
+package com.haguma.FuelConvert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelConvertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelConvertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haguma/FuelConvert/FuelConvertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haguma.FuelConvert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelConvertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
